10ab25cf9e7fcfc7cae6ac31bea7994f4817115f
Stephen Saunders <saunders.stephen@gmail.com>
2019-12-25 13:00:39 -0800

    Test: App initializes with `state.turnX` boolean

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -18,19 +18,26 @@ describe('<App />', () => {
     wrapper.unmount();
     toggleTimeTravelSpy.restore();
   })
-  it('initializes with `state.timeTravel` that is `false`', () => {
-    const timeTravel = wrapper.state().timeTravel;
-    expect(timeTravel).to.be.a('boolean', '`state.timeTravel` is not Boolean')
-    expect(timeTravel).to.be.false;
-  })
-  it('initializes with `state.history` that is an Array of Arrays', () => {
-    const history = wrapper.state().history;
-    expect(history instanceof Array, '`state.history` is not an Array').to.true
-    expect(history.length, '`state.history` should initialize with one inner Array').to.equal(1);
-    const initialGameState = history[0];
-    expect(initialGameState instanceof Array, '`state.history[0] ` is not an Array').to.be.true;
-    expect(initialGameState.length).to.equal(9, '`state.history[0]` must have a length of 9');
-    expect(initialGameState, '`state.history[0]` should initialize with null values').to.deep.equal(Array(9).fill(null))
+  describe(' state', () => {
+    it('initializes with `state.timeTravel` that is `false`', () => {
+      const timeTravel = wrapper.state().timeTravel;
+      expect(timeTravel).to.be.a('boolean', '`state.timeTravel` is not Boolean')
+      expect(timeTravel).to.be.false;
+    })
+    it('initializes with `state.history` that is an Array of Arrays', () => {
+      const history = wrapper.state().history;
+      expect(history instanceof Array, '`state.history` is not an Array').to.true
+      expect(history.length, '`state.history` should initialize with one inner Array').to.equal(1);
+      const initialGameState = history[0];
+      expect(initialGameState instanceof Array, '`state.history[0] ` is not an Array').to.be.true;
+      expect(initialGameState.length).to.equal(9, '`state.history[0]` must have a length of 9');
+      expect(initialGameState, '`state.history[0]` should initialize with null values').to.deep.equal(Array(9).fill(null))
+    })
+    it(' initilizes with `state.turnX` set to true', () => {
+      const turnX = wrapper.state().turnX;
+      expect(typeof turnX, '`turnX` should be a Boolean').to.equal('boolean')
+      expect(turnX, 'X should start the game').to.be.true;
+    })
   })
   describe('#render ', () => {
     let button;
