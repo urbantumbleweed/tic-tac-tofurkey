1a2c34799431b793478b283139777521305968c1
Stephen Saunders <saunders.stephen@gmail.com>
2019-12-24 19:35:42 -0800

    Add scss loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,17 @@ module.exports = {
             presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
       }
     ]
   },
