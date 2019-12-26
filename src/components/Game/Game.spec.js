aa436aaf6ca5fb87c1fe1d7dc3c6d12740a820fc
Stephen Saunders <saunders.stephen@gmail.com>
2019-12-26 11:20:25 -0800

    Test: capture console.error from proptype warnings

--- src/components/Game/Game.spec.js
+++ src/components/Game/Game.spec.js
@@ -2,21 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { expect } from 'chai';
 import { mount, shallow } from 'enzyme';
+import { mock } from 'sinon';
 import Game from './Game';
 import gameCombinator from 'test/gameCombinator';
-import Square from '../Square';
 
 const gameCombinations = gameCombinator();
 
 describe('<Game />', () => {
   let wrapper;
+  let mockConsole;
+  beforeEach(() => {
+    mockConsole = mock(console);
+  })
   afterEach(() => {
     wrapper = null;
+    mockConsole.restore();
+    mockConsole = null;
   })
   describe(' `#render()', () => {
     it(' renders null if no `game` is passed', () => {
+      const propError = mockConsole.expects("error");
+      if (process.env.MOCHA_WATCH) {
+        propError.atMost(1);
+      } else {
+        propError.once(1);
+      }
       wrapper = mount(<Game />);
-      expect(wrapper.isEmptyRender()).to.be.true
+      expect(wrapper.isEmptyRender()).to.be.true;
+      expect(propError.verify()).to.be.true
     })
     it(' mounts a div that has a class `gameboard`', () => {
       const game = gameCombinations.slice(0,1);
@@ -24,6 +37,7 @@ describe('<Game />', () => {
       expect(wrapper.find('.gameboard').exists()).to.be.true;
     })
     it(' renders 9 <Square /> for all 6046 valid game combinations', function () {
+      this.slow(10000)
       this.timeout(10000)
       gameCombinations.forEach((game, gameIndex) => {
         wrapper = shallow(<Game game={game} />);
