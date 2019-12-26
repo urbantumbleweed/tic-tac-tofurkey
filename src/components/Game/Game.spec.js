60521d165c7124d07aa69ac3f2ba6cd7ed7a18bc
Stephen Saunders <saunders.stephen@gmail.com>
2019-12-25 19:52:48 -0800

    Test: Game renders Squares for all valid games

--- src/components/Game/Game.spec.js
+++ src/components/Game/Game.spec.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { expect } from 'chai';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Game from './Game';
 import gameCombinator from 'test/gameCombinator';
+import Square from '../Square';
 
 const gameCombinations = gameCombinator();
 
@@ -21,6 +22,17 @@ describe('<Game />', () => {
       const game = gameCombinations.slice(0,1);
       wrapper = mount(<Game game={gameCombinations[0]} />);
       expect(wrapper.find('.gameboard').exists()).to.be.true;
+    })
+    it(' renders 9 <Square /> for all 6046 valid game combinations', (done) => {
+      gameCombinations.forEach((game, gameIndex) => {
+        wrapper = shallow(<Game game={game} />);
+        const gameboard = wrapper.find('.gameboard');
+        expect(gameboard.children().length).to.equal(9)
+        const children = gameboard.find('Square');
+        expect(children.length, 'children of `Game` should all be of type <Square>').to.equal(9)
+        wrapper.unmount();
+      })
+      done();
     })
   })
 })
