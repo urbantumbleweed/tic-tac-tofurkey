73f0c88e6903431df1e54e77188429fd29537282
Stephen Saunders <saunders.stephen@gmail.com>
2019-12-26 17:53:37 -0800

    `calculateWinner` works on bottom row

--- src/components/App/App.helpers.js
+++ src/components/App/App.helpers.js
@@ -2,10 +2,16 @@ import { validMessages } from './App.constants';
 
 export function promptMap(moves, winner = null) {
   switch (true) {
+    case ('X wins', winner === 'X'):
+      return validMessages[2];
+    case ('O wins', winner === 'O'):
+      return validMessages[3];
     case ('X turn', moves < 9 && moves % 2 === 0):
       return validMessages[0];
     case ('O Turn', moves < 9 && moves % 2 === 1):
       return validMessages[1];
+    case ('Tie', moves === 9 && !winner):
+      return validMessages[4];
     default:
       return '';
   }
@@ -14,11 +20,12 @@ export function promptMap(moves, winner = null) {
 export function calculateWinner(game) {
   const wins = [
     [0, 1, 2],
-    [3, 4, 5]
+    [3, 4, 5],
+    [6, 7, 8]
   ];
   for(var i = 0; i < wins.length; i++) {
     const [a, b, c] = wins[i];
-    if (game[a] === game[b] && game[b] === game[c]) {
+    if (game[a] && game[a] === game[b] && game[a] === game[c]) {
       return game[a];
     }
   }
