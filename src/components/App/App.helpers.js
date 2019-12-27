72cf9025d3157e049cf5ccac5fe13c743934b27c
Stephen Saunders <saunders.stephen@gmail.com>
2019-12-26 17:58:43 -0800

    `calculateWinner` mid column

--- src/components/App/App.helpers.js
+++ src/components/App/App.helpers.js
@@ -22,7 +22,8 @@ export function calculateWinner(game) {
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
-    [0, 3, 6]
+    [0, 3, 6],
+    [1, 4, 7],
   ];
   for(var i = 0; i < wins.length; i++) {
     const [a, b, c] = wins[i];
