5d71569d4bc41c9f4ba95c2ab7ee4a305cc7451c
Stephen Saunders <saunders.stephen@gmail.com>
2019-12-26 17:22:21 -0800

    `calculateWinner` detects mid-row wins

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -85,6 +85,37 @@ describe('<App />', () => {
         expect(wrapper.state().moves, 'the length of moves should increase after click').to.deep.equal(moves.concat([nextMove]));
         expect(wrapper.state().winner, '`state.winner` should indicate the winner').to.deep.equal('O');
       })
+      it('detects a middle-row winner', () => {
+        const [ game, nextMove, moves] = wins.midRowX;
+        wrapper.setState({
+          game,
+          moves,
+        })
+        expect(wrapper.state().game).to.deep.equal(game);
+        expect(wrapper.state().moves).to.deep.equal(moves);
+        wrapper.find('.square').at(nextMove).simulate('click');
+        // since game is a reference to old state, we can mutate it to include the value added on click
+        game.splice(nextMove, 1, 'X')
+        expect(wrapper.state().game, '`state.game` after click should include new move').to.deep.equal(game);
+        expect(wrapper.state().moves, 'the length of moves should increase after click').to.deep.equal(moves.concat([nextMove]));
+        expect(wrapper.state().winner, '`state.winner` should indicate the winner').to.deep.equal('X');
+      })
+      it('detects a mid-row O winner', () => {
+        const [ game, nextMove, moves] = wins.midRowO;
+        wrapper.setState({
+          game,
+          moves,
+        })
+        expect(wrapper.state().game).to.deep.equal(game);
+        expect(wrapper.state().moves).to.deep.equal(moves);
+        wrapper.find('.square').at(nextMove).simulate('click');
+        // since game is a reference to old state, we can mutate it to include the value added on click
+        game.splice(nextMove, 1, 'O')
+        expect(wrapper.state().game, '`state.game` after click should include new move').to.deep.equal(game);
+        expect(wrapper.state().moves, 'the length of moves should increase after click').to.deep.equal(moves.concat([nextMove]));
+        expect(wrapper.state().winner, '`state.winner` should indicate the winner').to.deep.equal('O');
+      })
+      })
     })
   })
   describe(' state', () => {
