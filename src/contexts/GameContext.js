d2b8586124e60f93bcbfbad524139b173fb79a1c
Stephen Saunders <saunders.stephen@gmail.com>
2019-12-28 17:24:47 -0800

    GameContext defintion

--- src/contexts/GameContext.js
+++ src/contexts/GameContext.js
@@ -4,6 +4,9 @@ export const GameContext = React.createContext({
   timeTravel: false,
   goTo: () => {},
   makeMove: () => {},
+  moves: [],
 });
+
+GameContext.displayName = 'GameContext';
 
 export default GameContext;
