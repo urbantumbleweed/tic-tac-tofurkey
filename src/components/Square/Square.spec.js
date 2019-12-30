828219bd9aeac0d8b80fc8666000b8b32834763c
Stephen Saunders <saunders.stephen@gmail.com>
2019-12-29 16:32:00 -0800

    Test: <Square /> uses emojis

--- src/components/Square/Square.spec.js
+++ src/components/Square/Square.spec.js
@@ -22,4 +22,20 @@ describe('<Square />', () => {
   it('one child is a div with the class `value`', () => {
     expect(wrapper.find('div.square').find('.value')).to.have.lengthOf(1);
   })
+  it('displays a `🍗` for `X`', () => {
+    const valueProp = wrapper.getElement().props.value;
+    expect(valueProp).to.equal('X');
+    expect(wrapper.text()).to.equal(`🍗`);
+  })
+  it('displays a `🍞` for `O`', () => {
+    wrapper.unmount();
+    wrapper = mount(<Square
+      key={1}
+      value={'O'}
+      index={1}
+    />);
+    const valueProp = wrapper.getElement().props.value;
+    expect(valueProp).to.equal('O');
+    expect(wrapper.text()).to.equal(`🍞`);
+  })
 })
