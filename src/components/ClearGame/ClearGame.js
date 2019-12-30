b2d0fc6a20e7d6fde1b8a051421d936dbe573a92
Stephen Saunders <saunders.stephen@gmail.com>
2019-12-29 16:18:45 -0800

    ClearButton styles

--- src/components/ClearGame/ClearGame.js
+++ src/components/ClearGame/ClearGame.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
 
 class ClearGame extends Component {
   render() {
     return (
-      <div role="button" onClick={this.props.onClick}>
+      <div
+        role="button"
+        onClick={this.props.onClick}
+        className={classnames('button', 'clearButton')}
+      >
         Clear Game
       </div>
     );
--- src/components/ClearGame/ClearGame.spec.js
+++ src/components/ClearGame/ClearGame.spec.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { spy } from 'sinon';
 import ClearButton from '.';
 
 describe('<ClearButton />', () => {
   it('has a class `button.clearButton`', () => {
     const wrapper = shallow(<ClearButton />);
+    expect(wrapper.hasClass('button')).to.be.true;
+    expect(wrapper.hasClass('clearButton')).to.be.true;
     expect(wrapper.find('.button.clearButton')).to.have.lengthOf(1);
+  })
+  it('has an `onClick`', () => {
+    const onClick = spy();
+    const wrapper = shallow(<ClearButton onClick={onClick}/>);
+    expect(wrapper.getElement().props.onClick).to.exist;
+    wrapper.simulate('click');
+    expect(onClick.callCount).to.equal(1);
   })
 })
