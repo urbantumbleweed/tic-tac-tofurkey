c100430e01596e84ae0c3b9d97a0f2440a1639c8
Stephen Saunders <saunders.stephen@gmail.com>
2019-12-29 16:59:02 -0800

    Square has value dependent style

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -1,5 +1,6 @@
 import React, { Component, useContext } from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import validOptions from 'src/constants/validOptions'
 import GameContext from 'contexts/GameContext';
 import { emojify } from 'helpers';
@@ -8,7 +9,16 @@ import Move from '../Move';
 function Square ({ index, value }) {
   const { makeMove, timeTravel} = useContext(GameContext);
   return (
-    <div className="square" onClick={() => makeMove(index)}>
+    <div
+      className={classnames(
+        'square',
+        {
+          xValue: value === 'X',
+          oValue: value === 'O',
+        }
+      )}
+      onClick={() => makeMove(index)}
+    >
       <div className="value">{emojify(value)}</div>
       {timeTravel && <Move square={index} />}
     </div>
